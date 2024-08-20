0ee4ea2b3c2e37f3a3a968ceb4ff82f52b81f1c5
fatmahussein <fartunhussein346@gmail.com>
2024-08-20 15:24:47 +0300

    fix linter error

--- app/models/visible.rb
+++ app/models/visible.rb
@@ -1,7 +1,7 @@
 module Visible
     extend ActiveSupport::Concern
   
-    VALID_STATUSES = ['public', 'private', 'archived']
+    VALID_STATUSES = [ 'public', 'private', 'archived']
   
     included do
       validates :status, inclusion: { in: VALID_STATUSES }
@@ -16,5 +16,5 @@ module Visible
     def archived?
       status == 'archived'
     end
-  end
+end
   
